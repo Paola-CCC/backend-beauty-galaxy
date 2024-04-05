46c4829ebf92dd4620f3d6757a5f7be89cf515bd
Paola_C <paola.cyprien@hetic.net>
2024-04-05 22:45:00 +0200

    clean

--- src/Utils/config.php
+++ src/Utils/config.php
@@ -6,11 +6,3 @@ define("DB_CHARSET", "utf8");
 define("DB_USER", "u574090949_user");
 define("DB_PASSWORD", "Password7845+");
 define("JWT_SECRET", "unSuperSecret");
-
-
-// define("DB_HOST", "database");
-// define("DB_NAME", "galaxy_beauty_db");
-// define("DB_CHARSET", "utf8");
-// define("DB_USER", "root");
-// define("DB_PASSWORD", "password");
-// define("JWT_SECRET", "unSuperSecret");
