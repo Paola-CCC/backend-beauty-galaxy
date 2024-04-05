c4b15e1d35ac2757f294d2260f2678852a6dbb15
Paola_C <paola.cyprien@hetic.net>
2024-04-06 00:29:20 +0200

    changer nom database

--- src/Utils/config.php
+++ src/Utils/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define("DB_HOST", "database");
-define("DB_NAME", "galaxy_beauty_db");
+define("DB_NAME", "u574090949_galaxy_beauty");
 define("DB_CHARSET", "utf8");
 define("DB_USER", "root");
 define("DB_PASSWORD", "password");
